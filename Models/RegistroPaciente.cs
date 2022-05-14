87bde17724732948582c71cf44b0126c7eaf319f
Juan L. Restituyo <juanluisrestituyo@gmail.com>
2022-05-14 10:35:04 -0400

    Agregando un modelo y enviando un formulario

---
+++ Models/RegistroPaciente.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace HistoriaClinica.Models
+{
+    public class RegistroPaciente
+    {
+        public string nombre_usuario { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string confirmar_password { get; set; }
+        public string recordarme { get; set; }
+    }
+}
