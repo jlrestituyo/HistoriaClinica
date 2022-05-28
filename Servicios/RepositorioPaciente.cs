9bffbe226971515231f255f80e055dedc261770f
Juan L. Restituyo <juanluisrestituyo@gmail.com>
2022-05-28 10:28:44 -0400

    agregando servicio de Pacientes y modificando puerto de lanzamiento de la aplicacion

---
+++ Servicios/RepositorioPaciente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Dapper;
+using HistoriaClinica.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace HistoriaClinica.Servicios
+{
+    public interface IRepositorioPaciente {
+        void RegistrarPaciente(RegistroPaciente registroPaciente);
+    }
+
+    public class RepositorioPaciente: IRepositorioPaciente
+    {
+
+        private readonly string connectionString;
+
+        public RepositorioPaciente(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnectio");
+        }
+
+        public void RegistrarPaciente(RegistroPaciente registroPaciente)
+        {
+            using (var conexion = new SqlConnection(connectionString))
+            {
+                conexion.Query($@"insert into RegistroPaciente " +
+                    "(username, email, password, confirmacion)" +
+                    "values" +
+                    "(@nombre_usuario, @email, @password, @confirmacion) ", registroPaciente);
+            }
+        }
+    }
+}
